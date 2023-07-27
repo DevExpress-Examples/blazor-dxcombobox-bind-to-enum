7bacbc950d753ff683995a62aec525d8a0825bd2
elenapeskova <Elena.Peskova@devexpress.com>
2023-07-27 11:33:56 +0400

    add code

---
+++ CS/Data/Education.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace Education.Data {
+    public enum EducationType {
+        [Display(Name = "Not Stated")]
+        NoInfo = 0,
+        [Display(Name = "High School")]
+        School = 1,
+        [Display(Name = "College")]
+        College = 2,
+        [Display(Name = "University Degree")]
+        UniversityDegree = 3,
+        [Display(Name = "PhD")]
+        PhD = 4
+    }
+
+    public class EducationDegree {
+        public EducationType Value { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public static class Extensions {
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue)
+                where TAttribute : Attribute => enumValue.GetType()
+                                                         .GetMember(enumValue.ToString())
+                                                         .First()
+                                                         .GetCustomAttribute<TAttribute>();
+    }
+}
